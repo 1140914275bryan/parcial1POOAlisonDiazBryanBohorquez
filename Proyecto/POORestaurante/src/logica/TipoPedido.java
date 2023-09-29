63c5e948e3867d4f43812b791664e80919daa0da
1140914275bryan <145615740+1140914275bryan@users.noreply.github.com>
2023-09-28 21:40:00 -0500

    Add files via upload

---
+++ Proyecto/POORestaurante/src/logica/TipoPedido.java
@@ -0,0 +1,47 @@
+package logica;
+
+import java.util.ArrayList;
+
+public class TipoPedido {
+
+	private int numeroespecialidad;
+	private String nombre;
+	private ArrayList<Pedido> pedidos;
+	
+	public int getId() {
+		return numeroespecialidad;
+	}
+	public void setId(int numeroespecialidad) {
+		this.numeroespecialidad = numeroespecialidad;
+	}
+	
+	public int getNumeroespecialidad() {
+		return numeroespecialidad;
+	}
+	public void setNumeroespecialidad(int numeroespecialidad) {
+		this.numeroespecialidad = numeroespecialidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<Pedido> getPedidos() {
+		return pedidos;
+	}
+	public void setPedidos(ArrayList<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	public TipoPedido(int numeroespecialidad, String nombre) {
+		this.numeroespecialidad = numeroespecialidad;
+		this.nombre = nombre;
+		this.pedidos = new ArrayList<Pedido>();
+	}
+	public void ingresarPedido(int numerodeeleccion, String nombre2, int precioVenta, int cantidad) {
+		Pedido pedido = new Pedido(numerodeeleccion, nombre,precioVenta, cantidad);
+		this.pedidos.add(pedido);		
+	}	
+	
+}
