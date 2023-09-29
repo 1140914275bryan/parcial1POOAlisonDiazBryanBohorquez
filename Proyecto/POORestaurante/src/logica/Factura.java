79cce452c0827e8ed38dc74f483a59ecc8793c3f
1140914275bryan <145615740+1140914275bryan@users.noreply.github.com>
2023-09-29 18:27:49 -0500

    Add files via upload

---
+++ Proyecto/POORestaurante/src/logica/Factura.java
@@ -0,0 +1,76 @@
+package logica;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Factura {
+	
+	private int numero;
+	private Date hora;
+	private Date fecha;
+	private int valorTotal;
+	private float descuento;
+	private Cajero cajero;
+	private ArrayList<FacturaPedido> facturaPedidos;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Date getHora() {
+		return hora;
+	}
+	public void setHora(Date hora) {
+		this.hora = hora;
+	}
+	public int getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(int valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public Cajero getCajero() {
+		return cajero;
+	}
+	public void setCajero(Cajero cajero) {
+		this.cajero = cajero;
+	}
+	public ArrayList<FacturaPedido> getFacturaPedidos() {
+		return facturaPedidos;
+	}
+	public void setFacturaProductos(ArrayList<FacturaPedido> facturaPedidos) {
+		this.facturaPedidos = facturaPedidos;
+	}
+	public Factura(int numero, Date fecha, Cajero cajero) {
+		super();
+		this.fecha = fecha;
+		this.cajero = cajero;
+		this.facturaPedidos = new ArrayList<FacturaPedido>();
+	}
+	public void calcularTotal() {
+		int total = 0;
+		for(FacturaPedido facturaPedido : this.facturaPedidos) {
+			total += facturaPedido.getPrecio() * facturaPedido.getCantidad();
+		}
+		this.valorTotal = total;
+	}
+	public void adicionarPedido(Pedido pedido, int cantidad) {
+		FacturaPedido facturaPedido = new FacturaPedido(cantidad, pedido.getPrecioVenta(), pedido);
+		this.facturaPedidos.add(facturaPedido);		
+	}
+	public float getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+	}
+}
